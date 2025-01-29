5a59358366abfe5fb630eb145db22f135eb05a31
Akshay-Syscort <akshay.shaharwale@syscort.com>
2025-01-29 18:34:21 +0530

    link filter in custom user profile  doctype

--- custom_site1/custom_site1/doctype/custom_user_profile/custom_user_profile.js
+++ custom_site1/custom_site1/doctype/custom_user_profile/custom_user_profile.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2025, me and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Custom User Profile", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Custom User Profile", {
+  refresh(frm) {
+    frm.fields_dict.category.get_query = function (doc) {
+      return {
+        filters: {
+          data: "two",
+        },
+      };
+    };
+  },
+});
